d38d5f574b1e3b4c1d8b14f92ab64872550371c2
Andres Roa <andresmore1423@hotmail.com>
2022-10-15 00:37:15 -0500

    Incorporación de la carga de imagenes a los estaticos public

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@
 import express from "express";
 import morgan from 'morgan'
 import mongoose from 'mongoose';
+import multer from "multer";
+import path from 'path'
+import {fileURLToPath} from 'url';
+
+
 
 // import for dev
 import { routerUser, routerTask } from "./routes/index.routes.js";
@@ -11,8 +16,20 @@ import { routerUser, routerTask } from "./routes/index.routes.js";
 // instancias
 const app = express();
 
+// configuracion para que __dirname funcione como modulo
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// configuracion para subir las imagenes
+const storage = multer.diskStorage({
+    destination: path.join(__dirname, 'public/uploads'),
+    filename: (req, file, cb) => {
+        cb(null, new Date().getTime() + path.extname(file.originalname))
+    }
+})
 // midelwere morgan
 app.use(morgan('dev'))
+app.use(multer({storage}).single('image')) // Configuración y .single() recibe como parametro el nombre del input que enviara o cargara la imagen
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
